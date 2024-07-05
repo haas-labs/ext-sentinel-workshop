5de941ef17c3cf048368f0c17c4831bec3c20571
ausov-hacken <ausov@hacken.io>
2024-07-05 17:47:56 +0300

    feat: increase transfer monitor manifest version

--- monitor/manifest.py
+++ monitor/manifest.py
@@ -8,7 +8,7 @@ class Schema(BaseSchema):
 
 metadata = MetadataModel(
     name="Transfer Monitor",
-    version="0.1.1",
+    version="0.1.2",
     status=Status.ACTIVE,
     description=" ".join(
         [
